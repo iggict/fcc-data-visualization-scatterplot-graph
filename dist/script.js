a294d9cb774a4a9e1a690c1106b28c238c0dee82
iggict <58374304+iggict@users.noreply.github.com>
2021-11-18 18:22:41 +0100

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,14 +1,59 @@
+/** Set size */
+
+const margin = {
+  top: 100,
+  right: 20,
+  bottom: 30,
+  left: 60
+};
+
+const parentWidth = 920;
+const parentHeight = 630;
+
+const width = parentWidth - margin.left - margin.right;
+const height = parentHeight - margin.top - margin.bottom;
+
 /** Datetime functions */
 
 const parseTime = d3.timeParse("%M:%S");
 const formatTime = d3.timeFormat("%M:%S");
 
-/** D3 elements declaration */
-const tooltip = d3
+/** D3 containers */
+
+var graph = d3
   .select("body")
-  .append("div")
-  .attr("class", "tooltip")
-  .attr("id", "tooltip");
+  .append("svg")
+  .attr("id", "scatterplot_graph")
+  .attr("class", "graph")
+  .attr("width", parentWidth)
+  .attr("height", parentHeight)
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+const tooltip = d3
+  .select('body')
+  .append('div')
+  .attr('id', 'tooltip')
+  .attr('class', 'tooltip');
+
+const title = graph
+  .append('text')
+  .attr('id', 'title')
+  .attr('class', 'title')
+  .attr('x', width / 2)
+  .attr('y', 0 - margin.top / 2)
+  //.attr('text-anchor', 'middle')
+  //.style('font-size', '30px')
+  .text('titleee');
+
+  const subtitle = graph
+    .append('text')
+    .attr('class', 'subtitle')
+    .attr('x', width / 2)
+    .attr('y', 0 - margin.top / 2 + 25)
+    //.attr('text-anchor', 'middle')
+    //.style('font-size', '20px')
+    .text("Subtitleee");
 
 /** Tooltip inner HTML */
 
@@ -25,26 +70,28 @@ const tooltipInnerHtml = (item) => {
     <p>
       <span='tt-doping'>${item.Doping}</span>
     </p>`;
-};
+  }
 
 /** Load data */
 
 const JSONFile =
-  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
+"https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
 
 d3.json(JSONFile)
   .then((data) => {
+    
     /** Parse data */
-
-    data.forEach((d) => (d.Time = parseTime(d.Time)));
-
+    
+    data.forEach((d) => (d.Time = parseTime(d.Time)));  
+  
     /** Debugger */
-
+  
     const jsonDebug = d3
-      .select("body")
-      .append("div")
-      .attr("id", "debugger")
-      .html("<pre>" + JSON.stringify(data, null, 4) + "</pre>");
+       .select("body")
+       .append("div")
+       .attr("id", "debugger")
+       .html("<pre>" + JSON.stringify(data, null, 4) + "</pre>");
+  
   })
-  .catch((err) => console.error(err));
-
+    .catch((err) => console.error(err)
+);
